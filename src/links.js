3851a641afcd643c96ef509422fe9df4bd15109a
Jasper Speicher <jasper.speicher@gmail.com>
2024-09-29 17:12:21 -0700

    Build TOC

--- src/links.js
+++ src/links.js
@@ -3,7 +3,16 @@ import dispatcher from "./dispatcher";
 import d3 from "d3";
 
 class LinksSection {
-	init(containerNode, screenIsSmall) {}
+	init(containerNode, screenIsSmall) {
+		const scrollParent = $("#strategies-section .scroll-parent");
+		$("[data-scroll-to]").on("click", (e) => {
+			console.log(e.target);
+			const link = $(e.target);
+			const selector = link.data("scroll-to");
+			const destination = $(selector);
+			scrollParent.animate({ scrollTop: destination[0].offsetTop - 80 });
+		});
+	}
 
 	open() {}
 	close() {
