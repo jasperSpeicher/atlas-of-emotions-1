2e37d3d588551263077ab6a5b559213584b20cc6
Jasper Speicher <jasper.speicher@gmail.com>
2024-12-30 12:37:01 -0800

    Improve journal image layout

--- src/more-pages/annex-scientific-basis.js
+++ src/more-pages/annex-scientific-basis.js
@@ -1,17 +1,16 @@
-import d3 from 'd3';
-import _ from 'lodash';
+import d3 from "d3";
+import _ from "lodash";
 
-import * as utils from './utils.js';
-import dispatcher from '../dispatcher.js';
-import sassVars from '../../scss/variables.json';
+import * as utils from "./utils.js";
+import dispatcher from "../dispatcher.js";
+import sassVars from "../../scss/variables.json";
 
 export default {
-
 	isInited: false,
 	wrapper: null,
 
 	init: function (containerNode, data) {
-		this.data = data.annex['scientific-basis'];
+		this.data = data.annex["scientific-basis"];
 
 		this.wrapper = containerNode;
 
@@ -19,17 +18,23 @@ export default {
 		this.isInited = true;
 	},
 
-	setContent: function() {
+	setContent: function () {
 		if (!this.wrapper) return;
-		
+
 		// intro
-		this.wrapper.appendChild(utils.makeTable(this.data.desc, []));
+		const intro = document.createElement("div");
+		intro.classList.add("intro");
+		const image = document.createElement("img");
+		image.src = "img/journal.png";
+		intro.appendChild(utils.makeTable(this.data.desc, []));
+		intro.appendChild(image);
+		this.wrapper.appendChild(intro);
 		// survey results
-		this.wrapper.appendChild(utils.makeTable(this.data.contentIntro, this.data.content));
+		this.wrapper.appendChild(
+			utils.makeTable(this.data.contentIntro, this.data.content)
+		);
 		// footer content
 		this.wrapper.appendChild(utils.makeTable(this.data.footer[0], []));
 		this.wrapper.appendChild(utils.makeTable(this.data.footer[1], []));
 	},
-
-
 };
